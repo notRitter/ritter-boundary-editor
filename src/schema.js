ed5159ff5ff90d144c36bbd3069407a34de32926
notRitter <39321560+notRitter@users.noreply.github.com>
2025-12-22 22:57:35 -0500

    Update schema.js

--- src/schema.js
+++ src/schema.js
@@ -1,19 +1,27 @@
-const Schema = {
-  version: 1,
-  boundaryTemplate() {
-    return {
-      name: "New Boundary",
-      type: "spawn", // spawn | unspawn
-      maps: [],
-      distance: 4,
-      thickness: 1,
-      updateMode: "Movement",
-      wait: 0,
-      maxEvents: 0,
-      enabled: true,
-      preloads: [],
-      boundaryList: []
-    };
-  }
-};
+function createEmptyBoundary() {
+  return {
+    name: "",
+    kind: "spawn",
+    maps: [],
 
+    size: {
+      mode: "streaming",
+      distance: 2,
+      width: null,
+      height: null
+    },
+
+    thickness: 1,
+    type: "FillOn",
+    updateMode: "Movement",
+    wait: 10,
+    maxEvents: 400,
+    enabled: true,
+
+    autoHandler: {
+      enabled: true,
+      spawnMap: 1,
+      boundaries: []
+    }
+  };
+}
